b711cad06d6c587314350b7cab8099d67aed0b36
ocinpp <ocin25@gmail.com>
2018-12-15 03:30:09 +0800

    docs: add docstring
    Add docstring to methods

--- data/process_data.py
+++ data/process_data.py
@@ -3,59 +3,66 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
+    """
+    Load the messages from messages_filepath and the
+    categories from categories_filepath
+    """
     # load messages
     messages = pd.read_csv(messages_filepath)
-    
+
     # load categories
     categories = pd.read_csv(categories_filepath)
-    
+
     # merge datasets to a data frame
     df = messages.merge(categories, left_on='id', right_on='id', how='outer')
-    
+
     return df
 
 def clean_data(df):
-    """  
-    Split categories into separate category columns
-    Convert category values to just numbers 0 or 1        
     """
-    
+    Clean the dataframe by:
+    1. Split categories into separate category columns
+    2. Convert category values to just numbers 0 or 1
+    3. Remove duplicated values
+    """
+
     # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(';', expand=True)
-    
+
     # select the first row of the categories dataframe
     row = categories[:1].copy()
-    
+
     # use this row to extract a list of new column names for categories
     category_colnames = row.apply(lambda x : x[0][:-2])
-    
+
     # rename the columns of `categories`
     categories.columns = category_colnames
-    
-    for column in categories:    
+
+    for column in categories:
         # set each value to be the last character of the string
         categories[column] = categories[column].apply(lambda x: x[-1:])
-    
+
         # convert column from string to numeric
         categories[column] = categories[column].apply(lambda x : int(x))
-    
+
     # drop the original categories column from `df`
     df = df.drop(['categories'], axis=1)
-    
+
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories], axis=1)
-    
+
     # drop duplicates
     df = df.drop_duplicates()
-    
+
     return df
-    
+
 def save_data(df, database_filename):
-    """ 
-    Save the clean dataset into an sqlite database   
+    """
+    Save the clean dataframe into an sqlite database
+    specified by database_filename
     """
     engine = create_engine('sqlite:///' + database_filename)
-    df.to_sql('Message', engine, index=False)  
+    df.to_sql('Message', engine, index=False)
 
 def main():
     if len(sys.argv) == 4:
@@ -68,12 +75,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
